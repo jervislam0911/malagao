f5df66be86d2a82254553c32d82beddac9c4910b
Jervis <lin.jervis@yahoo.com>
2018-03-24 21:44:33 -0400

    add email verification and image banner

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import RegexValidator
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 
 
 class UserProfile(AbstractUser):
@@ -34,3 +35,35 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    SEND_CHOICES = (
+        ("register", "register"),
+        ("forget", "forget")
+    )
+    code = models.CharField(max_length=20, verbose_name="verification code")
+    email = models.EmailField(max_length=50, verbose_name="email")
+    send_type = models.CharField(choices=SEND_CHOICES, max_length=10)
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = "email verification code"
+        verbose_name_plural = verbose_name
+
+# 轮播图model
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name="title")
+    image = models.ImageField(
+        upload_to="banner/%Y/%m",
+        verbose_name=u"Banner",
+        max_length=100)
+    url = models.URLField(max_length=200, verbose_name="url")
+    index = models.IntegerField(default=100, verbose_name="index")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"add time")
+
+    class Meta:
+        verbose_name = "Banner"
+        verbose_name_plural = verbose_name
